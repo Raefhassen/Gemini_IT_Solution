38e8680083342695ab5df3a4c49edbd429a6a30f
Raef Hassen <raef_hassen1978@live.com>
2019-12-16 00:41:54 +0100

    V2_lite

---
+++ GeminiITSAMM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GeminiITSAMM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
